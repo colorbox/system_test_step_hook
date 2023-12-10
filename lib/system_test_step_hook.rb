f60dc86c1074c2432169aed29dc0e795b2aa53fb
colorbox <colorbox222@gmail.com>
2023-12-10 18:09:48 +0900

    i

---
+++ lib/system_test_step_hook.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "system_test_step_hook/version"
+
+module SystemTestStepHook
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "system_test_step_hook"
+
+require "minitest/autorun"
